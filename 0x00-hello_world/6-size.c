e7229dc7f2e3f7f476029bc3baed047f68c42d98
sojames <wokteche@gmail.com>
2021-06-19 08:47:11 -0700

    know the size of various types on your local machine

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main (void)
+
 {
-printf("size on c ");
-printf(" ");
-printf(" ");
-printf(" ");
-printf(" ");
-printf(" ");
+/* int i;
+* float f;
+* char c;
+* double d ;
+* long double ld;
+* long int li;
+*/
+
+int i;
+float f;
+char c;
+double d ;
+long double ld;
+long int li;
+
+printf("The size of integer on my machine in bytes is: %lu \n", (unsigned long)sizeof(i) );
+printf("The size of float on my machine in bytes is: %lu\n", (unsigned long)sizeof(f) );
+printf("The size of char on my machine in bytes is: %lu\n", (unsigned long)sizeof(c) );
+printf("The size of double on my machine  in bytes is: %lu\n", (unsigned long)sizeof(d) );
+printf("The size of long double on my machine in bytes is: %lu \n", (unsigned long)sizeof(ld) );
+printf("The size of long int on my machine in bytes is: %lu \n", (unsigned long)sizeof(li) );
 
 	return(0);
 }
