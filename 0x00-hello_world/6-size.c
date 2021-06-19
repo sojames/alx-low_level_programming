9d479e4af6f4ddb6c2afad814660c549d4f0457e
sojames <wokteche@gmail.com>
2021-06-19 09:54:03 -0700

    this is to check the code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,25 +5,14 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
-
+int main(void)
 {
-/**
-* int i;
-* float f;
-* char c;
-* double d ;
-* long double ld;
-* long int li;
-*/
 
-printf("The size of integer on my machine in bytes is: %d \n", (unsigned int)sizeof(int) );
-printf("The size of float on my machine in bytes is: %d \n", (unsigned int)sizeof(float) );
-printf("The size of char on my machine in bytes is: %d \n", (unsigned int)sizeof(char) );
-printf("The size of double on my machine  in bytes is: %d \n", (unsigned int)sizeof(double) );
-printf("The size of long double on my machine in bytes is: %d \n", (unsigned int)sizeof(long double) );
-printf("The size of long int on my machine in bytes is: %d \n", (unsigned int)sizeof(long int) );
-printf("The size of long long int on my machine in bytes is: %d \n", (unsigned int)sizeof(long long int) );
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	return(0);
+return (0);
 }
