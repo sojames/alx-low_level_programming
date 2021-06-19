59e18ef4810af18a1366c2763f855dad7ad535b9
sojames <wokteche@gmail.com>
2021-06-19 09:46:24 -0700

    size on my machine edicted and compilled

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,22 @@
 int main (void)
 
 {
+/**
+* int i;
+* float f;
+* char c;
+* double d ;
+* long double ld;
+* long int li;
+*/
 
-printf("The size of integer on my machine in bytes is: %d \n", sizeof(int) );
-printf("The size of float on my machine in bytes is: %d \n", sizeof(float) );
-printf("The size of char on my machine in bytes is: %d \n", sizeof(char) );
-printf("The size of double on my machine  in bytes is: %d \n", sizeof(double));
-printf("The size of long double on my machine in bytes is: %d \n", sizeof(long double);
-printf("The size of long int on my machine in bytes is: %d \n", sizeof(long int));
+printf("The size of integer on my machine in bytes is: %d \n", (unsigned int)sizeof(int) );
+printf("The size of float on my machine in bytes is: %d \n", (unsigned int)sizeof(float) );
+printf("The size of char on my machine in bytes is: %d \n", (unsigned int)sizeof(char) );
+printf("The size of double on my machine  in bytes is: %d \n", (unsigned int)sizeof(double) );
+printf("The size of long double on my machine in bytes is: %d \n", (unsigned int)sizeof(long double) );
+printf("The size of long int on my machine in bytes is: %d \n", (unsigned int)sizeof(long int) );
+printf("The size of long long int on my machine in bytes is: %d \n", (unsigned int)sizeof(long long int) );
 
-return(0);
+	return(0);
 }
-
