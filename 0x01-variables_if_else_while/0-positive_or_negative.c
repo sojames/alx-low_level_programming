1f82ec94930e619dd8ced9b8b5738f507ef2ec7b
sojames <wokteche@gmail.com>
2021-06-19 17:48:23 -0700

    check if c programming is wide space collapsing not accepted

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,15 @@
 #include <time.h>
 #include <stdlib.h>
 /**
-* main - assigns a random number to int n everytime 
-* it executes, and prints it
-* Return: Always 0 (Success)
-*/
+ * main - assigns a random number to int n everytime 
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
-srand (time(0));
-n = rand() - RAND_MAX / 2 ;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
