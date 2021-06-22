b81cf01d4ff9d3ae1d255ab5690a01a7777ea3d6
sojames <wokteche@gmail.com>
2021-06-21 18:31:24 -0700

    nested loop test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar("\n");
+
+
+return (0);
+}
