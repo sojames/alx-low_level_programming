a5664b7af42b2e4da915bc400e807478e1c776cb
sojames <wokteche@gmail.com>
2021-06-25 13:40:21 -0700

    hello all

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,45 +1,14 @@
 #include "holberton.h"
 /**
- * print_most_numbers - print numbers
- *
- * Return: Always 0.
+ * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
+ * Return: Nothing
  */
 void print_most_numbers(void)
 {
+	int i;
 
-	int i; /*Declaring statements*/
-
-	i = 0;
-	while (i < 10)/*Start While*/
-	{
-		if (i != 2 && i != 4) /* No print 2 and 4*/
-		{
-			_putchar (i + '0'); /*print value i*/
-		}
-		i++;
-	}
-
-	_putchar ('\n'); /*New line*/
-}#include "holberton.h"
-/**
- * print_most_numbers - print numbers
- *
- * Return: Always 0.
- */
-void print_most_numbers(void)
-{
-
-	int i; /*Declaring statements*/
-
-	i = 0;
-	while (i < 10)/*Start While*/
-	{
-		if (i != 2 && i != 4) /* No print 2 and 4*/
-		{
-			_putchar (i + '0'); /*print value i*/
-		}
-		i++;
-	}
-
-	_putchar ('\n'); /*New line*
+	for (i = 0; i < 10; i++)
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	_putchar(i);
 }
