26352c5028dbd0823b029a6a743d10622a6efaae
sojames <wokteche@gmail.com>
2021-06-27 15:10:16 -0700

    hello pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+    int n=98; 
+    int *p = &n;
+
+
+}
