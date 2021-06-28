a0941d01df49b45bd3f280e6352075094bdc88f1
sojames <wokteche@gmail.com>
2021-06-27 23:18:13 -0700

    point 2 pjt

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *int _strlen() prototype
+ *function check and return length of string
+ *
+ *Return: Always return 0 after successful execution
+ */
+int _strlen(char *s)
+{ 
+	str[] = "Holberton!";
+
+       return(strlen(str));
+
+}
