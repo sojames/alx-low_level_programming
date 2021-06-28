0cbb294cede10d2cafbc55fe5db39e810ffa45b5
sojames <wokteche@gmail.com>
2021-06-27 22:54:45 -0700

    check point 01

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,10 +7,10 @@
  *pointer to an int as parameter and updates the value it points to to 98.
  * 
  * @n: pointer will be uodated
- * return void .
+ * return: void .
  */
 void reset_to_98(int *n)
 {
-*n = 98;
+	* n = 98;
 
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,12 @@ include "holberton.h"
  * main - check the code for ALX School students.
  * @a:updared to b
  * @b: updated to a
- * return void*
+ * return: void*
  */
 void swap_int(int *a, int *b)
 {
-int a;
-int b;
-*a=b;
-*b=a;
+	int z;
+	z =*a
+	*a=*b;
+	*b=z;
 }
