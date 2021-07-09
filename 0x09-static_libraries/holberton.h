33883f174dc76ae9fb8a2171d2960f5fa70357cf
sojames <wokteche@gmail.com>
2021-07-09 09:17:20 -0700

    define function and end if

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,4 +1,6 @@
-#!/bin/bash
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
 int _putchar(char c);                             void print_alphabet(void);                        void print_alphabet_x10(void);                    int _islower(int c);                              int _isalpha(int c);                              int print_sign(int n);                            int _abs(int n);                                  int print_last_digit(int n);                      void jack_bauer(void);                            void times_table(void);                           int add(int a, int b);                            void print_to_98(int n);                          void print_times_table(int n);
 
 
@@ -9,4 +11,5 @@ void positive_or_negative(int i);                 int largest_number(int a, int 
 int _putchar(char c);                             int _isupper(int c);                              int _isdigit(int c);                              int mul(int a, int b);                            void print_numbers(void);                         void print_most_numbers(void);                    void more_numbers(void);                          void print_line(int n);                           void print_diagonal(int n);                       void print_square(int size);                      void print_triangle(int size);
 
 int _putchar(char c);                             char *_strcat(char *dest, char *src);             char *_strncat(char *dest, char *src, int n);     char *_strncpy(char *dest, char *src, int n);     int _strcmp(char *s1, char *s2);                  void reverse_array(int *a, int n);                char *string_toupper(char *);                     char *cap_string(char *);                         char *leet(char *);                               char *rot13(char *);                              void print_number(int n);
+#endif
 
