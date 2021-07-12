58e6ce06d614ba1ff74bd4c77fe437b87209d32c
sojames <wokteche@gmail.com>
2021-07-12 13:55:45 -0700

    hello

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,31 +13,26 @@
 
 int main(int argc, char *argv[])
 {
-int i, T, c, a;
-i = T = c = a = 0;
-int coin [] = {25, 10, 5, 2, 1};
+int i, n, s, ch = 0;
+int c[] = {25, 10, 5, 2, 1};
+
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-T = atoi(argv[i]);
-if (T <= 0)
-{
+s = atoi(argv[1]);
+if (s < 1)
 printf("0\n");
-return (0);
-}
-while (coin[i] != 0)
+else
 {
-if (T >= coin[i])
+for (i = 0; i < 5 && s; i++)
 {
-a = (T / coin[i]);
-c += a;
-T -= coin[i] * a;
+n = s / c[i];
+ch += n;
+s -= n *c[i];
 }
-i++;
+printf("%d\n", ch);
 }
-printf("%d\n", c);
-
 return (0);
 }
