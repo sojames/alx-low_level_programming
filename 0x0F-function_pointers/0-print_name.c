cf168ae5fde137258694d9366f188bcb7abfd6fc
sojames <wokteche@gmail.com>
2021-07-21 14:27:42 -0700

    function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include"function_pointers.h"
+/**
+ * print_name - function that print name
+ * @name: name to be printed in a function
+ * @f: void functio
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+f(name);
+
+}
