c2368e5526f60c1a2d74b2cf87f3310ceb490566
sojames <wokteche@gmail.com>
2021-07-21 17:50:49 -0700

    last

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,16 +10,16 @@
 
 int main(int argc, char *argv[])
 {
-	register int i, num;
-	char *ptr = (char *)main;
+register int i, num;
+char *ptr = (char *)main;
 
-	if (argc != 2)
-		printf("Error\n"), exit(1);
-	num = atoi(argv[1]);
-	if (num < 0)
-		printf("Error\n"), exit(2);
-	for (i = 0; i < num - 1; i++)
-		printf("%02hhx ", ptr[i]);
-	printf("%02hhx\n", ptr[i]);
-	return (0);
+if (argc != 2)
+printf("Error\n"), exit(1);
+num = atoi(argv[1]);
+if (num < 0)
+printf("Error\n"), exit(2);
+for (i = 0; i < num - 1; i++)
+printf("%02hhx ", ptr[i]);
+printf("%02hhx\n", ptr[i]);
+return (0);
 }
