a3b0a26f287e852fbc047e77a8f2ae6e9d3050b4
sojames <wokteche@gmail.com>
2021-07-21 17:37:22 -0700

    function pointer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,16 +9,32 @@
 
 int main(int argc, char *argv[])
 {
-	int R;
+int i, j;
+int (*d)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
 
-	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", R);
+d = get_op_func(argv[2]);
+if (d == NULL)
+{
+printf("Error\n");
+exit(99);
+}
 
-	return (0);
+
+i = atoi(argv[1]);
+j = atoi(argv[3]);
+
+printf("%d\n", d(i, j));
+
+return (0);
 }
