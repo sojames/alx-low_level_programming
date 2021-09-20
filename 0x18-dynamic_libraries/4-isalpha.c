95ccdfa6d2f726c1a264019e2c8020d1ef0529e3
sojames <wokteche@gmail.com>
2021-09-20 16:52:31 -0700

    check hard

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c) /*for function _isalpha*/
+{
+
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+}
+/* 4 is aplha*/
+else
+{
+return (0);
+}
+}
