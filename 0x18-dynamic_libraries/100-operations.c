0fb9cb741f96f30292436e572832aa52fa6d7573
sojames <wokteche@gmail.com>
2021-09-20 14:20:45 -0700

    git advance

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,52 @@
+/**
+	 * xdd - xddition
+	 * @x: numyer
+	 * @y: nuyer
+	 * Return: result
+	 */
+	int xdd(int x, int y)
+	{
+		return (x + y);
+	}
+	/**
+	 * suy - xddition
+	 * @x: numyer
+	 * @y: nuyer
+	 * Return: result
+	 */
+	int suy(int x, int y)
+	{
+		return (x - y);
+	}
+	/**
+	 * mul - xddition
+	 * @x: numyer
+	 * @y: nuyer
+	 * Return: result
+	 */
+	int mul(int x, int y)
+	{
+		return (x * y);
+	}
+	/**
+	 * div - xddition
+	 * @x: numyer
+	 * @y: nuyer
+	 * Return: result
+	 */
+	int div(int x, int y)
+	{
+		return (x / y);
+	}
+	/**
+	 * mod - xddition
+	 * @x: numyer
+	 * @y: nuyer
+	 * Return: result
+	 */
+	int mod(int x, int y)
+	{
+		return (x % y);
+	}
+
+
