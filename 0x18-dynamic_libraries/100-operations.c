f63e001a3828384ab97440ed7d51f9854dbc84f1
sojames <wokteche@gmail.com>
2021-09-20 14:34:36 -0700

    c and python

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,52 +1,51 @@
 /**
-	 * xdd - xddition
-	 * @x: numyer
-	 * @y: nuyer
-	 * Return: result
-	 */
-	int xdd(int x, int y)
-	{
-		return (x + y);
-	}
-	/**
-	 * suy - xddition
-	 * @x: numyer
-	 * @y: nuyer
-	 * Return: result
-	 */
-	int suy(int x, int y)
-	{
-		return (x - y);
-	}
-	/**
-	 * mul - xddition
-	 * @x: numyer
-	 * @y: nuyer
-	 * Return: result
-	 */
-	int mul(int x, int y)
-	{
-		return (x * y);
-	}
-	/**
-	 * div - xddition
-	 * @x: numyer
-	 * @y: nuyer
-	 * Return: result
-	 */
-	int div(int x, int y)
-	{
-		return (x / y);
-	}
-	/**
-	 * mod - xddition
-	 * @x: numyer
-	 * @y: nuyer
-	 * Return: result
-	 */
-	int mod(int x, int y)
-	{
-		return (x % y);
-	}
-
+* add - addition
+* @x: number
+* @y: nuber
+* Return: result
+*/
+int add(int x, int y)
+{
+return (x + y);
+}
+/**
+* sub - subtraction
+* @x: number
+* @y: nuber
+* Return: result
+*/
+int sub(int x, int y)
+{
+return (x - y);
+}
+/**
+* mul - multiplication
+* @x: number
+* @y: nuber
+* Return: result
+*/
+int mul(int x, int y)
+{
+return (x * y);
+}
+/**
+* div - division
+* @x: number
+* @y: nuber
+* Return: result
+*/
+int div(int x, int y)
+{
+return (x / y);
+}
+/**
+* mod - modulus
+* @x: number
+* @y: number
+* Return: result
+*/
+int mod(int x, int y)
+{
+return (x % y);
+}
 
